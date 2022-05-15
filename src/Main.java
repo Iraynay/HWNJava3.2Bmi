a146724615da4cb2c71b35cd2b4ea115d80da822
Ирина Пащенко <i.m.uglanova@gmail.com>
2022-05-15 14:24:51 +0300

    Main updated

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double weight = 57;
+        double weight = 67;
         double height = 168;
         double bmi = service.calculate(weight, height);
-        System.out.println(bmi);
+        System.out.println ("BMI = " + bmi);
     }
 
 }
